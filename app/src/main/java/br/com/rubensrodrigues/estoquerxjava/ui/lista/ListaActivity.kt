d851ed9d3b3dcd473c36bf50c4f9536af2d59eca
Rubens Rodrigues <rubensrodriguesv2@gmail.com>
2019-11-03 23:12:02 -0300

    Adapter do Recycler criado e mock para teste na activity

--- app/src/main/java/br/com/rubensrodrigues/estoquerxjava/ui/lista/ListaActivity.kt
+++ app/src/main/java/br/com/rubensrodrigues/estoquerxjava/ui/lista/ListaActivity.kt
@@ -6,13 +6,28 @@ import android.util.Log
 import android.view.*
 import android.widget.Toast
 import br.com.rubensrodrigues.estoquerxjava.R
+import br.com.rubensrodrigues.estoquerxjava.model.Produto
 import br.com.rubensrodrigues.estoquerxjava.ui.toast
+import kotlinx.android.synthetic.main.activity_lista.*
 
 class ListaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista)
+
+        val adapter = ListaAdapter(this)
+        adapter.produtos = listOf(
+            Produto(nome ="Playstation 4", preco = 1200f, quantidade = 10),
+            Produto(nome ="iPhone X", preco = 4000f, quantidade = 10),
+            Produto(nome ="Xbox One", preco = 1100f, quantidade = 12),
+            Produto(nome ="Asus Zenfone Max Pro M1", preco = 800f, quantidade = 50),
+            Produto(nome ="TV Samsung 42", preco = 1500f, quantidade = 13),
+            Produto(nome ="Laptop DELL i7", preco = 2400f, quantidade = 5),
+            Produto(nome ="Playstation 4 Pro", preco = 2200f, quantidade = 10),
+            Produto(nome ="Pirulito", preco = 0.25f, quantidade = 10)
+        )
+        listaProdutos.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/br/com/rubensrodrigues/estoquerxjava/ui/lista/ListaAdapter.kt
+++ app/src/main/java/br/com/rubensrodrigues/estoquerxjava/ui/lista/ListaAdapter.kt
@@ -1,23 +1,43 @@
 package br.com.rubensrodrigues.estoquerxjava.ui.lista
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import br.com.rubensrodrigues.estoquerxjava.R
+import br.com.rubensrodrigues.estoquerxjava.model.Produto
+import kotlinx.android.synthetic.main.item_produto.view.*
 
-class ListaAdapter: RecyclerView.Adapter<ListaAdapter.ItemViewHolder>() {
+class ListaAdapter(private val context: Context): RecyclerView.Adapter<ListaAdapter.ItemViewHolder>() {
+
+    var produtos: List<Produto> = listOf()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun getItemCount() = produtos.size
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        val itemView =
+            LayoutInflater.from(context).inflate(R.layout.item_produto, parent, false)
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return ItemViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.bind(produtos[position])
     }
 
     inner class ItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
 
+        fun bind(produto: Produto){
+            with(itemView){
+                itemNome.text = produto.nome
+                itemPreco.text = context.getString(R.string.preco_produto, produto.preco)
+                itemQuantidade.text = context.getString(R.string.quantidade_produto, produto.quantidade)
+            }
+        }
     }
 }
