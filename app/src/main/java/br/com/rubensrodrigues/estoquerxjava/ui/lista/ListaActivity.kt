d73ade5bfb5474c8db3e4b6892c3bb57a1e0b473
Rubens <rubens.rodrigues@mobile2you.com.br>
2019-12-12 19:14:34 -0300

    mini micro refatoracao

--- app/src/main/java/br/com/rubensrodrigues/estoquerxjava/ui/lista/ListaActivity.kt
+++ app/src/main/java/br/com/rubensrodrigues/estoquerxjava/ui/lista/ListaActivity.kt
@@ -77,7 +77,5 @@ class ListaActivity : AppCompatActivity(), ListaContract.View {
         toast(getString(R.string.erro_api, mensagem))
     }
 
-    override fun mostrarLista(produtos: List<Produto>) {
-        adapter.produtos = produtos
-    }
+    override fun mostrarLista(produtos: List<Produto>) { adapter.produtos = produtos }
 }
